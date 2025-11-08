f0013af791bd0ce9b17fd6369fc82e7317482c7a
wh3pp <wh3pp@proton.me>
2025-11-07 19:58:48 -0500

    Fix lspattach autocmd

--- lua/yanvim/core/autocmds.lua
+++ lua/yanvim/core/autocmds.lua
@@ -1,27 +1,28 @@
 local map = vim.keymap.set
 
-on_attach = function(_, bufnr)
-  local function opts(desc)
-    return { buffer = bufnr, desc = "LSP " .. desc }
-  end
+local on_attach = function(_, bufnr)
+  local function opts(desc) return { buffer = bufnr, desc = 'LSP ' .. desc } end
 
-  map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
-  map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
-  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
-  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+  map('n', 'gD', vim.lsp.buf.declaration, opts('Go to declaration'))
+  map('n', 'gd', vim.lsp.buf.definition, opts('Go to definition'))
+  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts('Add workspace folder'))
+  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts('Remove workspace folder'))
 
-  map("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts "List workspace folders")
+  map(
+    'n',
+    '<leader>wl',
+    function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+    opts('List workspace folders')
+  )
 
-  map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
-  map("n", "<leader>ra", require "nvchad.lsp.renamer", opts "NvRenamer")
+  map('n', '<leader>D', vim.lsp.buf.type_definition, opts('Go to type definition'))
 end
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-
-local autocmd = vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args) 
-    
+    on_attach(client, bufnr)
   end,
 })
