f2c85e8672d59296c61ff6a3102031726d9c92db
wh3pp <wh3pp@proton.me>
2025-11-07 19:48:20 -0500

    Add autocmds for lsp

---
+++ lua/yanvim/core/autocmds.lua
@@ -0,0 +1,27 @@
+local map = vim.keymap.set
+
+on_attach = function(_, bufnr)
+  local function opts(desc)
+    return { buffer = bufnr, desc = "LSP " .. desc }
+  end
+
+  map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
+  map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
+  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
+  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+
+  map("n", "<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts "List workspace folders")
+
+  map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
+  map("n", "<leader>ra", require "nvchad.lsp.renamer", opts "NvRenamer")
+end
+
+
+
+local autocmd = vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args) 
+    
+  end,
+})
