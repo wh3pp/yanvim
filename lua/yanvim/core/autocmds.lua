d35bfc6269fd4054ae677f92daddb35c86a631a5
wh3pp <wh3pp@proton.me>
2025-11-07 20:46:38 -0500

    add mappings for lsp functions

--- lua/yanvim/core/autocmds.lua
+++ lua/yanvim/core/autocmds.lua
@@ -2,26 +2,30 @@ local map = vim.keymap.set
 
 local on_attach = function(_, bufnr)
   local function opts(desc) return { buffer = bufnr, desc = 'LSP ' .. desc } end
+  -- Navigation
+  map('n', 'gd', vim.lsp.buf.definition, opts('Go to Definition'))
+  map('n', 'gD', vim.lsp.buf.declaration, opts('Go to Declaration'))
+  map('n', '<leader>D', vim.lsp.buf.type_definition, opts('Go to Type Definition'))
+  map('n', 'gi', vim.lsp.buf.implementation, opts('Go to Implementation'))
+  map('n', 'gr', vim.lsp.buf.references, opts('Find All References'))
 
-  map('n', 'gD', vim.lsp.buf.declaration, opts('Go to declaration'))
-  map('n', 'gd', vim.lsp.buf.definition, opts('Go to definition'))
-  map('n', 'gi', vim.lsp.buf.implementation, opts('Go to implementation'))
-  map('n', 'gr', vim.lsp.buf.references, opts('Go to references'))
-  map('n', '<leader>de', vim.diagnostic.open_float, opts('Show line diagnostics'))
-  map('n', '<leader>dl', vim.diagnostic.setloclist, opts('List all diagnostics'))
-  map('n', '<leader>ca', vim.lsp.buf.code_action, opts('Code action'))
-  map('n', '<leader>ra', vim.lsp.buf.rename, opts('Rename'))
-  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts('Add workspace folder'))
-  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts('Remove workspace folder'))
+  -- Inspection
+  map('n', 'K', vim.lsp.buf.hover, opts('Hover Documentation'))
+  map('si', '<C-k>', vim.lsp.buf.signature_help, opts('Signature Help'))
 
+  -- Actions
+  map('n', '<leader>ra', vim.lsp.buf.rename, opts('Rename Symbol'))
+  map('n', '<leader>ca', vim.lsp.buf.code_action, opts('Code Action'))
+
+  -- Workspace management
+  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts('Add Workspace Folder'))
+  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts('Remove Workspace Folder'))
   map(
     'n',
     '<leader>wl',
     function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-    opts('List workspace folders')
+    opts('List Workspace Folders')
   )
-
-  map('n', '<leader>D', vim.lsp.buf.type_definition, opts('Go to type definition'))
 end
 
 vim.api.nvim_create_autocmd('LspAttach', {
--- lua/yanvim/core/keymaps.lua
+++ lua/yanvim/core/keymaps.lua
@@ -6,5 +6,8 @@ map('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file', silent = true })
 map('n', '<leader>q', '<cmd>q<cr>', { desc = 'Quit', silent = true })
 map('n', '<leader>wq', '<cmd>wq<cr>', { desc = 'Save and quit', silent = true })
 
+-- Diagnostics
+map('n', '<leader>de', vim.diagnostic.open_float, { desc = 'Show Line Diagnostics' })
+map('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'List All Diagnostics' })
 -- Clear search highlight
 map('n', '<esc>', '<cmd>nohlsearch<cr>')
