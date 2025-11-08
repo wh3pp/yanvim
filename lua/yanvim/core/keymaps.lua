fb807ad06cd3c89c83a3feae8bbca801b27172ad
wh3pp <wh3pp@proton.me>
2025-11-07 19:49:15 -0500

    Add keymap for search highlight

--- lua/yanvim/core/keymaps.lua
+++ lua/yanvim/core/keymaps.lua
@@ -11,3 +11,6 @@ map('n', '<leader>ya', '<cmd>%y+<cr>', { desc = 'Yank entire file' })
 map('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file', silent = true })
 map('n', '<leader>q', '<cmd>q<cr>', { desc = 'Quit', silent = true })
 map('n', '<leader>wq', '<cmd>wq<cr>', { desc = 'Save and quit', silent = true })
+
+-- Clear search highlight
+map('n', '<esc>', '<cmd>nohlsearch<cr>')
